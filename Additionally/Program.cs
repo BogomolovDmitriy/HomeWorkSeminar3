93a8e2e9a54487ace2c35ab3ceff4dcb9b2da59c
Dmitriy <dimgod80@yandex.ru>
2022-10-28 17:14:08 +0800

    Add Additionally

---
+++ Additionally/Program.cs
@@ -0,0 +1,27 @@
+﻿void GetPrime(int num)
+{
+    int IsPrime(int number)
+    {
+        for (int i = 2; i < number; i++)
+        {
+            if (number % i == 0)
+            {
+                return -1;
+            }
+        }
+        Console.Write(number + " ");
+        return number;
+    }
+    
+    for (int i = 2; i <= num; i++)
+    {
+        IsPrime(i);
+    }
+}
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+GetPrime(N);
+
+
